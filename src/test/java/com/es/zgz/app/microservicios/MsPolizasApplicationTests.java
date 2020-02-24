291d52be34bceeae6a0115300d608a646c4b922f
michael <michael.g.aguilar.p@gmail.com>
2020-02-24 20:54:37 +0100

    Proyecto Microservicios Spring

---
+++ src/test/java/com/es/zgz/app/microservicios/MsPolizasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es.zgz.app.microservicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPolizasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
